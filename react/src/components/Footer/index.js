f42451151dc41931caffec17b1c9ca1db4a0e9eb
Isabelydjoliz <isabelydjoliz@gmai.com>
2024-08-15 10:42:45 -0300

    banner

---
+++ react/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por Isabely D'joliz</h2>
+        </footer>
+    );
+}
+
+export default Footer;
